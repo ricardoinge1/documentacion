87c0ae5b98bac1270dee1293dcd8621c485ace5d
Ricardo Rubio <ricardorubio@MacBook-Air-de-Ricardo.local>
2025-09-12 10:11:05 -0600

    elenixpestaña

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -88,7 +88,6 @@ const config: Config = {
           to: 'http://localhost:3000/',
           label: 'Regresar a Elenix',
           position: 'left',
-          target: "_self",
         },
         {
           type: 'localeDropdown',
